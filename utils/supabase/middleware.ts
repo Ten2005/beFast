0e7ddd2356976ee23e0d6c92435e61c69b36642c
Ten Suzuki <122023835+Ten2005@users.noreply.github.com>
2025-11-05 13:50:20 +0900

    middleware.ts を更新。認証情報がある時はダッシュボードにリダイレクト

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -45,9 +45,17 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/signup")
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/signup";
+    return NextResponse.redirect(url);
+  } elif (
+    user &&
+    request.nextUrl.pathname !== "/" ||
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/signup")
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = "/dashboard";
     return NextResponse.redirect(url);
   }
 
