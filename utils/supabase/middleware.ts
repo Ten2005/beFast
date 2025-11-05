07982c2b620ec3aab6e1cc7ca5b08ff32ccee2e5
Ten Suzuki <122023835+Ten2005@users.noreply.github.com>
2025-11-05 13:55:31 +0900

    middleware.ts を更新。エラー修正

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -48,11 +48,13 @@ export async function updateSession(request: NextRequest) {
     const url = request.nextUrl.clone();
     url.pathname = "/signup";
     return NextResponse.redirect(url);
-  } elif (
+  } else if (
     user &&
+    (
     request.nextUrl.pathname !== "/" ||
     request.nextUrl.pathname.startsWith("/login") ||
     request.nextUrl.pathname.startsWith("/signup")
+    )
   ) {
     const url = request.nextUrl.clone();
     url.pathname = "/dashboard";
